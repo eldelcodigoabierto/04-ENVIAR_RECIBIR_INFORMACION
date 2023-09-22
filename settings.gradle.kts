d5c2c2ce1b86699bcd7376351a6b534f8ad3c402
alexroig <alex.roigronda@cieep.com>
2023-09-22 10:10:25 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "04-ENVIAR Y RECIBIR INFORMACION"
+include(":app")
+ 
